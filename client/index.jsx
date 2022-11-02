94b33e336292ef7a6ce21d47db9142daf0cbc5b9
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-11-02 13:50:03 +0100

    Step 5 - A bit of code cleanup

--- client/index.jsx
+++ client/index.jsx
@@ -2,20 +2,7 @@ import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { ChatApp } from "./chatApp"
 
-const initialMessages = [
-    {
-        user: "User1",
-        message: "Message 1 from main",
-    },
-    {
-        user: "User2",
-        message: "Message 2",
-    },
-    {
-        user: "User1",
-        message: "Message 3",
-    },
-];
+const initialMessages = [];
 
 function UserRegistrationForm( { onUsername }) {
     const [ username, setUsername ] = useState("");
